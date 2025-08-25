97ef3858885e2b3ab6f0ab019d4f63c97a33ac6f
MohdAhzan <gsikahzanpkm@gmail.com>
2025-08-25 21:43:03 +0530

    changed db from sqllite to postgres added postgres volume in dockerfile

--- phoenix_mart/settings.py
+++ phoenix_mart/settings.py
@@ -100,7 +100,11 @@ SITE_ID = 1
 DATABASES = {
     'default': {
         'ENGINE': os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
-        'NAME': BASE_DIR / os.getenv("DB_NAME", "db.sqlite3"),
+        'NAME': os.getenv("DB_NAME",""),
+        'USER': os.getenv("DB_USER", ""),
+        'PASSWORD': os.getenv("DB_PASSWORD", ""),
+        'HOST': os.getenv("DB_HOST", ""),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
 
